56b15b2d9c99529d51ce0110b5503ccecb94766b
attharmirza <atthar.mirza@gmail.com>
2017-10-18 10:17:00 -0400

    Modified Touch Events
    Fine tuned mobile touch events, the buttons are much more usable from a
    UX perspective.

--- scripts/myGrid.js
+++ scripts/myGrid.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+  // Mobile Listeners
+  var windowWidth = $(window).width();
+  console.log(windowWidth);
+
   $('.grid').masonry({
     itemSelector: '.grid-item',
     columnWidth: 150,
@@ -12,18 +16,24 @@ $(document).ready(function() {
     }
   });
 
-  $('.grid-item').mouseover(function() {
-    $(this).find('.projectLabel').fadeIn(300);
-    $(this).find('.projectCover').css('opacity', 0.5);
-  });
-  $('.grid-item').mouseleave(function() {
-    $(this).find('.projectLabel').stop().hide();
-    $(this).find('.projectCover').css('opacity', 1);
-  });
-
-  $('.grid-item').on('touchstart', function() {
-    $(this).find('.projectLabel').fadeIn(300);
-    $(this).find('.projectCover').css('opacity', 0.5);
-  });
+  if (windowWidth > 600) {
+    $('.grid-item').mouseover(function() {
+      $(this).find('.projectLabel').fadeIn(300);
+      $(this).find('.projectCover').css('opacity', 0.5);
+    });
+    $('.grid-item').mouseleave(function() {
+      $(this).find('.projectLabel').stop().hide();
+      $(this).find('.projectCover').css('opacity', 1);
+    });
+  } else {
+    $('.grid-item').on('touchstart', function() {
+      $(this).find('.projectLabel').fadeIn(300);
+      // $(this).find('.projectCover').css('opacity', 0.5);
+    });
+    $('.grid-item').on('touchend', function() {
+      $(this).find('.projectLabel').delay(3000).fadeOut(300);
+      // $(this).find('.projectCover').delay(1000).css('opacity', 1);
+    });
+  }
 
 });
