6af68192fdf2686d87aa7380ab32d698fecb94bd
attharmirza <atthar.mirza@gmail.com>
2017-10-19 16:17:31 -0400

    Finished styling the "News" Page
    I added a lot of styling to the page to make it more interesting,
    including drop caps and a story viewer. The section is also very mobile
    responsive, with the full screen version having an index and viewer and
    the mobile version being a collapsable list.

--- scripts/myNews.js
+++ scripts/myNews.js
@@ -1,6 +1,34 @@
 $(document).ready(function() {
-  $('.storyThumb').click(function() {
-    // $('.newsReader').css('display', 'none');
-    $(this).find('.newsReader').toggle();
-  });
+
+  var windowWidth = $(window).width();
+
+  if (windowWidth < 1100) {
+
+    $('.storyThumb').click(function() {
+      $('.newsReader').hide();
+      $('.storyThumb').css('box-shadow', 'initial');
+      $(this).find('.newsReader').show();
+      $(this).css('box-shadow', 'inset 0 0 0 3px #e50f41');
+    });
+
+  } else {
+
+    var clicked = false;
+
+    $('.storyThumb').click(function() {
+      $('.storyThumb').css('box-shadow', 'initial');
+
+      $('.newsTab').find('.newsReader').remove();
+
+      postContent = $(this).find('.newsReader').clone();
+      $('.newsTab').append(postContent);
+      $('.newsTabHeader').hide();
+      postContent.show();
+
+      $(this).css('box-shadow', 'inset 0 0 0 3px #e50f41');
+      clicked = true;
+    });
+
+  }
+
 });
