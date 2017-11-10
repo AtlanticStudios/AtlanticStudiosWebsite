ecc31b146eb19637d96c9696fb13548a9f0ade01
attharmirza <atthar.mirza@gmail.com>
2017-11-10 18:05:59 -0500

    Finishing David's Notes
    The thumbnails on the watch page are now titled differently, and lots of
    other little refinements have been made across the site.

--- scripts/myGrid.js
+++ scripts/myGrid.js
@@ -18,22 +18,26 @@ $(document).ready(function() {
 
   if (windowWidth > 600) {
     $('.grid-item').mouseover(function() {
-      $(this).find('.projectLabel').fadeIn(300);
-      $(this).find('.projectCover').css('opacity', 0.5);
+      $(this).find('.projectLabel').show();
+      $(this).find('.projectGradient').show();
+      // $(this).find('.projectCover').css('opacity', 0.5);
     });
     $('.grid-item').mouseleave(function() {
       $('.projectLabel').stop().hide();
-      $(this).find('.projectCover').css('opacity', 1);
+      $(this).find('.projectGradient').hide();
+      // $(this).find('.projectCover').css('opacity', 1);
     });
   } else {
-    $('.grid-item').on('touchstart', function() {
-      $(this).find('.projectLabel').fadeIn(300);
-      // $(this).find('.projectCover').css('opacity', 0.5);
-    });
-    $('.grid-item').on('touchend', function() {
-      $('.projectLabel').delay(600).fadeOut(300);
-      // $(this).find('.projectCover').delay(1000).css('opacity', 1);
-    });
+    // $('.grid-item').on('touchstart', function() {
+    //   $(this).find('.projectLabel').show();
+    //   $(this).find('.projectGradient').show();
+    //   // $(this).find('.projectCover').css('opacity', 0.5);
+    // });
+    // $('.grid-item').on('touchend', function() {
+    //   $('.projectLabel').delay(600).fadeOut(300);
+    //   $(this).find('.projectGradient').delay(600).fadeOut(300);
+    //   // $(this).find('.projectCover').delay(1000).css('opacity', 1);
+    // });
   }
 
 });
